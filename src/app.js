42ff93313d78fbf4376bdf5e2e55b6c3a7cc653f
jayknight7535 <thewitchjay@gmail.com>
2025-04-24 21:38:16 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ let pronoun = ['the', 'our'];
+ let adj = ['great', 'big'];
+ let noun = ['jogger', 'racoon'];
+ for(i=0; i<pronoun.length; i++){
+  for(j=0; j<pronoun.length; j++){
+    for(k=0; k<noun.length; k++){
+     console.log(pronoun[i] + adj[j] + noun[k] + ".com")
+     }
+ }   
+} 
