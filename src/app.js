1a0aaa06fcc3b3f7d1cc48c63ed118f367879421
jayknight7535 <thewitchjay@gmail.com>
2025-04-25 23:07:17 +0000

    found a typo

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import "./assets/img/4geeks.ico";
  let adj = ['great', 'big'];
  let noun = ['jogger', 'racoon'];
  for(i=0; i<pronoun.length; i++){
-  for(j=0; j<pronoun.length; j++){
+  for(j=0; j<adj.length; j++){
     for(k=0; k<noun.length; k++){
      console.log(pronoun[i] + adj[j] + noun[k] + ".com")
      }
